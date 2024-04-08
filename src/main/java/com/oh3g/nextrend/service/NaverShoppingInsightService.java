6a9cd4f38bbc44b37416d70e955b90d10e76135c
Hyoungtaek <aqko0719@gmail.com>
2024-04-08 13:03:53 +0900

    feat: application.properties 수정 및 로컬 저장 해제 + 쇼핑인사이트 API 예제

---
+++ src/main/java/com/oh3g/nextrend/service/NaverShoppingInsightService.java
@@ -0,0 +1,6 @@
+package com.oh3g.nextrend.service;
+
+public class NaverShoppingInsightService {
+    private final String clientId = "{애플리케이션 등록 시 발급받은 클라이언트 아이디 값}";
+    private final String clientSecret = "{애플리케이션 등록 시 발급받은 클라이언트 시크릿 값}";
+}
