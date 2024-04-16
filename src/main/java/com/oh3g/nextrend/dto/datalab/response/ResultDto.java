2a109e4d72792946983568a8feb0c026f9697a8a
Hyoungtaek <aqko0719@gmail.com>
2024-04-16 12:27:50 +0900

    feat: dto 추가 (Serach, Datalab 구분)

---
+++ src/main/java/com/oh3g/nextrend/dto/datalab/response/ResultDto.java
@@ -0,0 +1,13 @@
+package com.oh3g.nextrend.dto.datalab.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class ResultDto {
+    private String title;
+    private String category;
+    private List<DataDto> data;
+}
