e3b58fcbd5ce208bd0b021df50f41c70f59d2fe5
Hyoungtaek <aqko0719@gmail.com>
2024-04-27 14:25:46 +0900

    feat: DatalabResponseDto 수정

--- src/main/java/com/oh3g/nextrend/dto/datalab/response/DatalabResponseDto.java
+++ src/main/java/com/oh3g/nextrend/dto/datalab/response/DatalabResponseDto.java
@@ -2,10 +2,12 @@ package com.oh3g.nextrend.dto.datalab.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class DatalabResponseDto {
     private String startDate;
     private String endDate;
     private String timeUnit;
-    private ResultDto results;
+    private List<ResultDto> results;
 }
