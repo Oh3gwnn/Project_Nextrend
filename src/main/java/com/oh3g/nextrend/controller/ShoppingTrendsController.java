82d60f73ce9d6890d34a8585922f86290c086012
Hyoungtaek <aqko0719@gmail.com>
2024-04-29 18:37:41 +0900

    feat: 반환 dto 수정(DatalabResponse -> InsightResult) - data는 비슷한데 검색 결과 message만 추가됨

--- src/main/java/com/oh3g/nextrend/controller/ShoppingTrendsController.java
+++ src/main/java/com/oh3g/nextrend/controller/ShoppingTrendsController.java
@@ -1,8 +1,8 @@
 package com.oh3g.nextrend.controller;
 
 import com.oh3g.nextrend.dto.datalab.request.DatalabRequestDto;
-import com.oh3g.nextrend.dto.datalab.response.ResultDto;
 import com.oh3g.nextrend.dto.datalab.response.DatalabResponseDto;
+import com.oh3g.nextrend.dto.datalab.result.InsightResultDto;
 import com.oh3g.nextrend.service.NaverShoppingInsightService;
 import com.oh3g.nextrend.service.SearchAnalysisService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,13 +19,17 @@ public class ShoppingTrendsController {
     private final SearchAnalysisService searchAnalysisService;
 
     @PostMapping("/shopping-trends")
-    public DatalabResponseDto postShoppingTrends(@RequestBody DatalabRequestDto datalabRequestDto) {
+    public InsightResultDto postShoppingTrends(@RequestBody DatalabRequestDto datalabRequestDto) {
         DatalabResponseDto responseEntity
                 = shoppingInsightService.callNaverShoppingTrendsAPI(datalabRequestDto);
         log.info(responseEntity.toString());
 
-        List<ResultDto> resultDto = responseEntity.getResults();
-        searchAnalysisService.calculateRatios(resultDto);
-        return responseEntity;
+        InsightResultDto insightResultDto = new InsightResultDto();
+        insightResultDto.setMessage(
+                searchAnalysisService.resultLocalDate(datalabRequestDto, responseEntity));
+        insightResultDto.setDatalabResponseDto(responseEntity);
+
+        log.info(insightResultDto.toString());
+        return insightResultDto;
     }
 }
